097825d428362853473e2aaaf00f3c3de9a5526c
Raymond Oung <r.oung@hapi-robo.com>
2018-10-25 11:00:58 +0900

    Adds nico-opendata dataset

--- resources/voice-statistics.sh
+++ resources/voice-statistics.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-echo "Extracting all data files..."
-
 cd voice-statistics.github.com/assets/data/
 
+echo "Download nico-opendata dataset..."
+wget https://nico-opendata.jp/assets/audio/2stack_voice_conversion/hiroshiba_normal.zip
+
+echo "Extracting all data files..."
 for filename in *.tar.gz
 do
-  tar xvzf $filename
+	tar xvzf $filename
+done
+
+for filename in *.zip
+do
+	unzip $filename
 done
 
 echo "Done!"
