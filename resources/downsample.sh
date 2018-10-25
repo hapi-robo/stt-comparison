20410fd9ac8edabf7e2a06281a679e303c175df4
Raymond Oung <r.oung@hapi-robo.com>
2018-10-25 12:48:01 +0900

    Adds downsampling script

---
+++ resources/downsample.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Usage: 
+#	./downsample {file} {(optional) sample_rate: 8000, 16000}
+
+file=$1
+extension="${file##*.}"
+filename="${file%.*}"
+bit_rate=16
+
+sample_rate=$2
+if [ -z $sample_rate ]; then sample_rate="16000"; fi
+
+out_file="${filename}_${sample_rate}.wav"
+
+sox ${file} -b ${bit_rate} -r ${sample_rate} -c 1 ${out_file}
+
+echo "${file} -> ${out_file}"
